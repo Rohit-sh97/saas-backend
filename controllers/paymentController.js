a18d0d7e2e7ff312752263c0de87831da70053ff
Rohit Sharma <rsantoshsharma9730@gmail.com>
2025-05-27 13:57:28 +0530

    updating payment route

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -21,10 +21,13 @@ exports.checkout = async (req, res) => {
 
 exports.verifyPayment = async (req, res) => {
   try {
+
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature, userId, planId, amount } = req.body;
+
     if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature) {
   return res.status(400).json({ message: 'Missing required fields' });
 }
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature, userId, planId, amount } = req.body;
+    
 
     const body = `${razorpay_order_id}|${razorpay_payment_id}`;
     const expectedSignature = crypto
@@ -39,15 +42,19 @@ exports.verifyPayment = async (req, res) => {
         orderId: razorpay_order_id,
         paymentId: razorpay_payment_id,
         amount,
-        status: 'active'
+        status: 'Active'
       });
 
       await subscription.save();
       res.status(200).json({ message: 'Payment verified', subscription });
     } else {
+      console.log("Expected:", expectedSignature);
+console.log("Received:", razorpay_signature);
+
       res.status(400).json({ message: 'Invalid signature' });
     }
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: 'Verification error' });
   }
 };
